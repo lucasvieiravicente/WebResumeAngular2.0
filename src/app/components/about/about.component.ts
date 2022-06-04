82b4c27abc73378249ed7f9bc7a25e32a071d820
Lucas Vieira Vicente <lucasvieiravicente1@gmail.com>
2022-06-04 02:24:51 -0300

    adjust on about component

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -6,9 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent {
+  socialMedias!: Readonly<Array<SocialMedia>>;
+
+  constructor() {
+    this.createSocialMedias();
+  }
 
   public getProfessionalTime(): number {
     return new Date().getFullYear() - 2017
   }
 
+  private createSocialMedias(): void {
+    this.socialMedias = [
+      new SocialMedia("https://www.linkedin.com/in/lucas-vieira-vicente", "fab fa-linkedin-in"),
+      new SocialMedia("https://github.com/lucasvieiravicente", "fab fa-github"),
+      new SocialMedia("https://api.whatsapp.com/send?phone=5511983773017&text=Olá%20,%20tudo%20bem?%20Te%20encontrei%20no%20seu%20currículo%20online", "fab fa-whatsapp")
+    ];
+  }
+
+}
+
+class SocialMedia {
+  url: string;
+  icon: string;
+
+  constructor(url: string, icon: string) {
+    this.url = url;
+    this.icon = icon;
+  }
 }
