926a9de84ebae0d0e4c6d1c35f82c3d14bf265ed
Lucas Vieira Vicente <lucasvieiravicente1@gmail.com>
2022-06-06 01:56:36 -0300

    update to angular 14 - part 3

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,6 +1,6 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { UntypedFormBuilder, UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms';
 import { ApiServicesService } from 'src/app/services/api-services.service';
 
 @Component({
@@ -10,11 +10,11 @@ import { ApiServicesService } from 'src/app/services/api-services.service';
 })
 export class ContactComponent implements OnInit {
 
-  public emailFormGroup!: FormGroup;
+  public emailFormGroup!: UntypedFormGroup;
   public disabledButton = false;
   public responseMessage = "";
 
-  constructor(private service: ApiServicesService, private formBuilder: FormBuilder) { }
+  constructor(private service: ApiServicesService, private formBuilder: UntypedFormBuilder) { }
 
   ngOnInit(): void {
     this.initForm();
@@ -22,11 +22,11 @@ export class ContactComponent implements OnInit {
 
   private initForm() {
     this.emailFormGroup = this.formBuilder.group({
-      name: new FormControl('', Validators.required),
-      email: new FormControl('', Validators.email),
-      phoneNumber: new FormControl('', [ Validators.pattern('\d'), Validators.maxLength(13)]),
-      body: new FormControl('', Validators.required),
-      subject: new FormControl('')
+      name: new UntypedFormControl('', Validators.required),
+      email: new UntypedFormControl('', Validators.email),
+      phoneNumber: new UntypedFormControl('', [ Validators.pattern('\d'), Validators.maxLength(13)]),
+      body: new UntypedFormControl('', Validators.required),
+      subject: new UntypedFormControl('')
     });
   }
 
